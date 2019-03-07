59cdc046b3130000634e4bcdf8709ab58ae1bad7
James Ault <james.ault@johnlewis.co.uk>
2019-03-07 15:50:38 +0000

    Linked js functions to interface using jQuery

---
+++ src/link.js
@@ -0,0 +1,54 @@
+$( document ).ready(function() {
+function updateTemperature () {
+  temperature = document.getElementById("temperature")
+  temperature.innerText = thermostat.temperature
+}
+function updateUsage () {
+  usage = document.getElementById("usage")
+  usage.innerText = thermostat.usage()
+  $('#usage').attr('class', thermostat.usage());
+}
+
+$( "#temperature-reset" ).click(function( event ) {
+  thermostat.reset(); console.log(1)
+  updateTemperature()
+  updateUsage()
+
+})
+$( "#temperature-up" ).click(function( event ) {
+  thermostat.up(); console.log(2)
+  updateTemperature()
+  updateUsage()
+
+})
+$( "#temperature-down" ).click(function( event ) {
+  thermostat.down(); console.log(3)
+  updateTemperature()
+  updateUsage()
+
+})
+$( "#powersaving-on" ).click(function( event ) {
+  thermostat.powersave('on'); console.log(4)
+  document.getElementById("power-saving-status").textContent = 'on'
+  updateTemperature()
+  updateUsage()
+
+})
+$( "#powersaving-off" ).click(function( event ) {
+  thermostat.powersave('off'); console.log(4)
+  document.getElementById("power-saving-status").textContent = 'off'
+  updateTemperature()
+  updateUsage()
+
+})
+var thermostat = new Thermostat();
+updateTemperature();
+updateUsage();
+  $( "#temperature-reset" ).click(function( event ) {
+
+      
+
+  });
+
+});
+
